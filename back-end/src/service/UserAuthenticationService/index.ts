3238a531a75d80d981b466c3074037ef3fdee322
hyarlei <hyarleysf@gmail.com>
2023-09-18 16:18:07 -0300

    User Authentication Service

---
+++ back-end/src/service/UserAuthenticationService/index.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+import { NextFunction, Request, Response } from 'express';
+import { sign } from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
+interface IUser {
+  email: string;
+  password: string;
+}
+
+export class UserAuthenticationService {
+  async execute({email, password}: IUser,
+    req: Request, res: Response, next: NextFunction ) {
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: 'User not exists' });
+    }
+
+    if(password !== user.password)res.status(400).json({ error: 'Password not match' });
+
+    const token = sign({
+        email: user.email,
+        password: user.password,
+        isAdmin: user.isAdmin,
+      },
+      'secret',
+      {
+        expiresIn: '1d',
+      },
+    );
+
+    return res.json({ token });
+  }
+}

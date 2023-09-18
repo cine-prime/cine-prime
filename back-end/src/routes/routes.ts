483f6afa26a23af4acf2f32720c2ca0b232ed289
hyarlei <hyarleysf@gmail.com>
2023-09-18 15:54:32 -0300

    Router User

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -3,9 +3,20 @@ import { Router } from "express";
 export const routes = Router();
 
 //User
-import { CreateUserController } from "../controllers/UserController";
+import { CreateUserController, DeleteUserController, ListarUsersController, ListarUsersEmailController, findUserById, UpdateUserController } from "../controllers/UserController";
 
 const createUserController = new CreateUserController();
+const deleteUserController = new DeleteUserController();
+const listarUsersController = new ListarUsersController();
+const listarUserController = new ListarUsersEmailController();
+const findUserByIdController = new findUserById();
+const updateUserController = new UpdateUserController();
 
 // Rotas Login
 routes.post("/user", createUserController.store);
+routes.delete("/user/:id", deleteUserController.delete);
+routes.get("/user", listarUsersController.index);
+routes.get("/user", listarUserController.listarUsers);
+routes.get("/user/:id", findUserByIdController.find);
+routes.put("/user/:id", updateUserController.update);
+

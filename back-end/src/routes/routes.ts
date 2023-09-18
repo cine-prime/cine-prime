8b070606193d7eba902ef478eac4aae49b4e8c77
hyarlei <hyarleysf@gmail.com>
2023-09-18 16:19:07 -0300

    Router Authentication

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -5,6 +5,9 @@ export const routes = Router();
 //User
 import { CreateUserController, DeleteUserController, ListarUsersController, ListarUsersEmailController, findUserById, UpdateUserController } from "../controllers/UserController";
 
+//Login
+import { AuthController } from "../controllers/LoginController";
+
 const createUserController = new CreateUserController();
 const deleteUserController = new DeleteUserController();
 const listarUsersController = new ListarUsersController();
@@ -12,11 +15,17 @@ const listarUserController = new ListarUsersEmailController();
 const findUserByIdController = new findUserById();
 const updateUserController = new UpdateUserController();
 
-// Rotas Login
+const authController = new AuthController();
+
+//Rotas User
 routes.post("/user", createUserController.store);
 routes.delete("/user/:id", deleteUserController.delete);
 routes.get("/user", listarUsersController.index);
 routes.get("/user", listarUserController.listarUsers);
 routes.get("/user/:id", findUserByIdController.find);
 routes.put("/user/:id", updateUserController.update);
+
+// Rotas de autenticação
+routes.post("/auth", authController.execute);
+
 

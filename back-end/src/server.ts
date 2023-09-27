b54c65469b938c647b393b5863bf739f3b0e1db2
Bruno Lima <brugit77@gmail.com>
2023-09-26 23:25:34 -0300

    Added "cors" to fix integration
    Tested integration between react front-end and express back-end.
    It was necessary install cors to avoid CORS-Problems during the fetch "Post" request

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { routes } from './routes/routes';
+import cors from 'cors';
 
 const app = express();
 const port = 3333;
+
+app.use(cors());
 
 app.use(express.json());
 
--- front-end/src/pages/Singup/index.js
+++ front-end/src/pages/Singup/index.js
@@ -1,5 +1,69 @@
-export default function Singup(props) {
+import React, { useState } from "react";
+
+export default function Signup() {
+  const [formData, setFormData] = useState({
+    email: "",
+    nome: "",
+    cpf: "",
+    telefone: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3333/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("User created", data);
+    } catch (error) {
+      console.error("Error during fetch operation", error.message);
+    }
+  };
+
   return (
-    <h1>tela de cadastro</h1>
-  )
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+      </label>
+      <br />
+      <label>
+        Nome:
+        <input type="text" name="nome" value={formData.nome} onChange={handleChange} required />
+      </label>
+      <br />
+      <label>
+        CPF:
+        <input type="text" name="cpf" value={formData.cpf} onChange={handleChange} required />
+      </label>
+      <br />
+      <label>
+        Telefone:
+        <input type="text" name="telefone" value={formData.telefone} onChange={handleChange} required />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 }

8bd11db27bdca8982556368f27a4504265eb5109
JorderGomes <jordernoveau@gmail.com>
2023-11-15 14:16:44 -0300

    find all sessions service

--- backend/src/service/SessionService/FindAllSessionsService/index.ts
+++ backend/src/service/SessionService/FindAllSessionsService/index.ts
@@ -3,14 +3,17 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-interface IRoom {
-    qtd_max: number;
-    typeExhibiyionAccepted: string;
+interface ISession {
+    dateTime: Date;
+    exibitionType: string;
+    dublingType: string;
+    idRoom: number;
+    idFilm: number;
 }
 
-export class ListRoomsService {
+export class ListSessionsService {
     async execute(_req: Request, res: Response){
-        const rooms = await prisma.room.findMany();
-        return res.json(rooms);
+        const sessions = await prisma.session.findMany();
+        return res.json(sessions);
     }
 }

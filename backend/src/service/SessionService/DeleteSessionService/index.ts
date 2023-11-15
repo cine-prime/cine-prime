db99bdeb027446fd78da4401535e0cf1f0fbdeca
JorderGomes <jordernoveau@gmail.com>
2023-11-15 14:14:54 -0300

    delete session service

--- backend/src/service/SessionService/DeleteSessionService/index.ts
+++ backend/src/service/SessionService/DeleteSessionService/index.ts
@@ -3,25 +3,25 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export class DeleteRoomService {
+export class DeleteSessionService {
   async execute( req: Request, res: Response) {
     const { id } = req.params;
     
-    const room = await prisma.room.findUnique({
+    const session = await prisma.session.findUnique({
       where: {
         id: Number(id),
       },
     });
-    if (room){
-      await prisma.room.delete({
+    if (session){
+      await prisma.session.delete({
         where: {
           id: Number(id),
         },
       });
     } else {
-      return res.json({ Message: "Sala inexistente" });
+      return res.json({ Message: "Sessão inexistente" });
     }
 
-    return res.json({ Message: "Sala removida com sucesso!" });
+    return res.json({ Message: "Sessão removida com sucesso!" });
   }
 }
